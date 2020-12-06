a57326f2e1caf7e41c4b705965e8e409731c49a3
Елисей Ли <elisey.li.2002@mail.ru>
2020-12-06 17:19:27 +1000

    in building

--- source/methods.cpp
+++ source/methods.cpp
@@ -1,10 +1,28 @@
+#include "structs.h"
 
 void Calculate(const char* str, float& result)
 {
-    unsigned short i = 0;
-    while (str[i] != '\0')
+    short i = 0;
+    short vent[MAX_STR_LENGTH / 2];
+    short indexVent = 0;
+    char stack[MAX_STR_LENGTH / 2];
+    short indexStack = 0;
+
+    // '0' = x30
+    // '0' = x39
+    // '*' = x2a
+    // '+' = x2b
+    // '-' = x2d
+    // '/' = x2f
+    while (str[i] != '\0' && i <= MAX_STR_LENGTH - 1)
     {
-        
+        if (str[i] >= '\x30' && str[i] <= '\x39')
+        {
+            vent[indexVent++] = (short)(str[i] - '0');
+        } else if (str[i] == '\x2a' || str[i] == '\x2b' || str[i] == '\x2d' || str[i] == '\x2f')
+        {
+            stack[indexStack++] = str[i];
+        }
         i++;
     }
 }
---
+++ source/structs.h
@@ -0,0 +1,3 @@
+#pragma once
+
+extern const int MAX_STR_LENGTH = 1024;
